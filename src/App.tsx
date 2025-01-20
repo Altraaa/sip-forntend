b7cf8fd27a98246df97b0a0ad824096c9ae74608
Aldi Saputra <aldimks7464@gmail.com>
2025-01-21 07:56:39 +0800

    Add Routing App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import TeacherPage from "./pages/TeacherPage";
 import ProfilePage from "./pages/Profile";
 import HelpPage from "./pages/HelpPage";
 import SettingsPage from "./pages/SettingsPage";
+import AddTaskPage from "./pages/AddTaskPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -18,9 +20,11 @@ function App() {
             <Route path="/class" element={<ClassPage />} />
             <Route path="/schedule" element={<SchedulePage />} />
             <Route path="/teacher" element={<TeacherPage />} />
+            <Route path="/addtask" element={<AddTaskPage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/help" element={<HelpPage />} />
             <Route path="/settings" element={<SettingsPage />} />
+            <Route path="login" element={<LoginPage />} />
           </Routes>
         </Router>
       </SidebarProvider>
